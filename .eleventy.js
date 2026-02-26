5c25fb0abd6d77ce03458dbac775199afbf97dfa
Gabri253 <gabriele.riccomini96@gmail.com>
2026-02-26 19:11:09 +0100

    Update configuration and documentation: enhance .eleventy.js, .gitignore, and README.md

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,4 @@
+/* Capire se inserirlo o no, attualmente non necessario. */
 //const plugins = require("@11ty/eleventy-navigation");
 
 module.exports = function(eleventyConfig) {
@@ -5,16 +6,18 @@ module.exports = function(eleventyConfig) {
   //eleventyConfig.addPlugin(plugins);
 
   // Copia i file statici
+  /* Perché dovrei mettere questa cartella e file che non esistono?
+    La prima cartella credo sia per il CSS */
   eleventyConfig.addPassthroughCopy("src/assets/");
   eleventyConfig.addPassthroughCopy("src/robots.txt");
 
   // Imposta le cartelle di input/output
   return {
     dir: {
-      input: "src",
-      output: "_site",
-      includes: "_includes",
-      layouts: "_includes/layouts"
+      input: "src", //Dove scrivo il codice
+      output: "_site", //Dove 11ty genera il sito
+      includes: "_includes", // ??
+      layouts: "_includes/layouts" //Sottocartella per i pezzi di layout (?)
     },
     templateFormats: ["md", "njk", "html"],
     markdownTemplateEngine: "njk",
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 ﻿# Heading
+
+**Per avviare il server:npx @11ty/eleventy --serve**
+
+**package-lock.json** --> Contiene l'elenco di tutti i pacchetti e le esatte versioni, in modo che quando eseguo il sito sul server vengano utilizzate le stesse esatte versioni.
+
+**package.json** --> Contiene le informazioni generiche sul sito, gli script da eseguire allo start, build, etc e le dipendenze da pacchetti esterni per la costruzione del sito
+
+**.eleventy.js** --> Aggiunge plugin, cartelle da cui pescare i file oltre a quelle di default, definisce le cartelle per costruire il sito. Sono le istruzioni per l'effettiva costruzione del sito.
+
+Il contenuto della cartella _site è l'output della build di 11ty.
+
+Definisco un file base, nel mio caso base.njk, che definisce la struttura del sito. YAML (---) per i file .md
+
+
